6e9df0eb789c829296da871dbcb624aa239b8d49
ststaynov <ststaynov@hotmail.com>
2016-08-01 15:14:16 +0300

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kiufte.wsgi --log-file -
---
+++ website/static/website/js/main.js
@@ -0,0 +1,25 @@
+
+window.onload = init;
+
+
+
+function init() {
+    var video = document.querySelector("#videoElement");
+    var constraints = {
+    video: { facingMode: { exact: "environment" } }
+  };
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia(constraints, handleVideo, videoError);
+    }
+
+    function handleVideo(stream) {
+      video.src = window.URL.createObjectURL(stream);
+    }
+
+    function videoError(e) {
+      alert(e.name);
+    }
+}
