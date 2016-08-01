3edc59ea7902ff7fc1b916666f51e961202d775a
ststaynov <ststaynov@hotmail.com>
2016-08-01 15:46:35 +0300

    ststic problem fix attemt #3

--- kiufte/settings.py
+++ kiufte/settings.py
@@ -126,7 +126,7 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'website/static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 ALLOWED_HOSTS = ['*']
 
 # Extra places for collectstatic to find static files.
