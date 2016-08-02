0b5c9fef34ea4228d7ce3a96bc5aa4e10caa4970
ststaynov <ststaynov@hotmail.com>
2016-08-02 19:38:43 +0300

    almost done

--- website/static/website/js/main.js
+++ website/static/website/js/main.js
@@ -21,7 +21,7 @@ function init() {
         } else if (sourceInfo.kind === 'video') {
             sourceID.push("" + sourceInfo.id);
             //alert('Pushed: ' +  sourceInfo.id);
-            $('.text').append(sourceInfo.id);
+            //$('.text').append(sourceInfo.id);
         } else {
           console.log('Some other kind of source: ', sourceInfo.id);
         }
@@ -54,5 +54,66 @@ function init() {
         function videoError(e) {
             alert(e.name);
         }
+    }
+
+//    Overlay code
+
+    var $nextBtn = $('.e-btn-next'),
+        count = 0,
+        $firstOverlay = $('.e-first-overlay'),
+        $secondOverlay = $('.e-second-overlay'),
+        $thirdOverlay = $('.e-third-overlay');
+    $('body').on('click', $nextBtn, function () {
+        count+=1;
+        console.log('click' + count);
+
+        switch (count) {
+            case 1:
+                $firstOverlay.addClass('move-out');
+                break;
+            case 2:
+                $secondOverlay.addClass('move-out');
+                break;
+            case 3:
+                $thirdOverlay.addClass('move-out');
+                $nextBtn.addClass('faded');
+                startLoadingCircle();
+                break;
+        }
+    })
+
+
+    function startLoadingCircle() {
+        var paths = document.getElementsByClassName('styled');
+        var path = paths[paths.length-1];
+        var counter = 0;
+        var total = 47;
+
+        var length = path.r.baseVal.value * 2 * Math.PI;
+
+        $(path).css({
+          'stroke-dasharray': length,
+          'stroke-dashoffset': 0,
+          'opacity': 1,
+        })
+
+        //simulate multiple requests completing at random times. setTimeout acts as success handler for ajax calls.
+        for (var i = 0; i < total; ++i) {
+          setTimeout( function() {
+            counter++;
+            var percentage = Math.ceil(100*counter/total);
+            $('.percentage').text(percentage)
+            $(path).css({
+              'stroke-dashoffset': -((counter/total)*length),
+            });
+
+            if (percentage >= 100) {
+              setTimeout(function() {
+              document.getElementById('progress').classList.add('complete'); document.body.classList.add('complete');
+              }, 500);
+            }
+          }, (Math.random() * 5000));
+        }
+
     }
 }
