0846092b46395c8326881abdd34fd563ca008eb3
ststaynov <ststaynov@hotmail.com>
2016-08-10 10:58:21 +0300

    link added

--- kiufte/urls.py
+++ kiufte/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'website.views.index'),
-    url(r'^$', 'website.views.responsibility'),
+    url(r'^responsibility/', 'website.views.responsibility'),
 ]
