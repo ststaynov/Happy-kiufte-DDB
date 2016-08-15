079f77836ea82e7274878489830b55c62208a048
ststaynov <ststaynov@hotmail.com>
2016-08-15 13:53:20 +0300

    Added text + scan target

--- website/static/website/js/main.js
+++ website/static/website/js/main.js
@@ -64,7 +64,7 @@ function init() {
         $secondOverlay = $('.e-second-overlay'),
         $thirdOverlay = $('.e-third-overlay'),
         $body = $('body');
-    $nextBtn.on('click tap', function () {
+    $nextBtn.on('click', function () {
         count+=1;
         console.log('click' + count);
 
@@ -83,7 +83,6 @@ function init() {
                 $body.addClass('no-overlay');
                 $body.removeClass('show-third');
                 $thirdOverlay.addClass('move-out');
-                //$nextBtn.addClass('faded');
                 startLoadingCircle();
                 break;
         }
@@ -123,34 +122,8 @@ function init() {
     })
 
     function startLoadingCircle() {
-        var paths = document.getElementsByClassName('styled');
-        var path = paths[paths.length-1];
-        var counter = 0;
-        var total = 147;
+      setTimeout( function() {
 
-        var length = path.r.baseVal.value * 2 * Math.PI;
-
-        $(path).css({
-          'stroke-dasharray': length,
-          'stroke-dashoffset': 0,
-          'opacity': 1,
-        });
-
-        //simulate multiple requests completing at random times. setTimeout acts as success handler for ajax calls.
-        for (var i = 0; i < total; ++i) {
-          setTimeout( function() {
-            counter++;
-            var percentage = Math.ceil(100*counter/total);
-            $('.percentage').text(percentage);
-            $(path).css({'stroke-dashoffset': -((counter/total)*length),});
-
-            if (percentage >= 100) {
-              setTimeout(function() {
-              document.getElementById('progress').classList.add('complete'); document.body.classList.add('complete');
-                $('.c-joke').addClass('show');
-              }, 500);
-            }
-          }, (Math.random() * 5000));
-        }
+      }, (Math.random() * 5000));
     }
 }
